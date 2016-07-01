82fc47aaf3a1495b56db732b65780b3658d282a9
Ariful Haque <arifulhb@gmail.com>
2016-07-01 22:21:04 +0800

    Updated Admin Sidemenu

--- resources/views/admin/templates/left-sidebar.blade.php
+++ resources/views/admin/templates/left-sidebar.blade.php
@@ -12,26 +12,20 @@
         </div>
 
         <!-- search form (Optional) -->
-    <form action="#" method="get" class="sidebar-form">
+    {{--<form action="#" method="get" class="sidebar-form">
         <div class="input-group">
         <input type="text" name="q" class="form-control" placeholder="Search Book...">
         <span class="input-group-btn">
         <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
         </span>
         </div>
-    </form>
+    </form>--}}
     <!-- /.search form -->
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">HEADER</li>
+            <li class="header">PAGES</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="{{ Request::is('admin') ? 'active' : '' }}">
-                <a href="{{ url('/admin') }}">
-                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                </a>
-            </li>
-
             <li class="treeview ">
                 <a href="#"><i class="fa fa-book"></i> <span>Books</span> <i
                             class="fa fa-angle-left pull-right"></i></a>
